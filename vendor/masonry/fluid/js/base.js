2485ddfa81e6dce5b67bd88510d6c5d075628138
Joshua Vaage <joshuavaage@gmail.com>
2013-05-21 12:58:36 -0700

    total restructuring brain bender

--- vendor/masonry/fluid/js/base.js
+++ vendor/masonry/fluid/js/base.js
@@ -1,32 +1,10 @@
-/**
- * Base js functions
- */
-
-$(document).ready(function(){
-    var $container = $('.container');
-
-    var gutter = 30;
-    var min_width = 170;
-    $container.imagesLoaded( function(){
-        $container.masonry({
-            itemSelector : '.box',
-            gutterWidth: gutter,
-            isAnimated: true,
-              columnWidth: function( containerWidth ) {
-                var box_width = (((containerWidth - 3*gutter)/4) | 0) ;
-
-                if (box_width < min_width) {
-                    box_width = (((containerWidth - gutter)/2) | 0);
-                }
-
-                if (box_width < min_width) {
-                    box_width = containerWidth;
-                }
-
-                $('.box').width(box_width);
-
-                return box_width;
-              }
-        });
-    });
+$('.container').masonry({
+  itemSelector: '.box',
+  // set columnWidth a fraction of the container width
+  columnWidth: function( containerWidth ) {
+    return containerWidth / 5;
+  }
 });
+
+
+
